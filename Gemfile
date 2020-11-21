7c11fc5076559e63ff81ae149e004b72cb41b80b
Maxime Lapointe <hunter_spawn@hotmail.com>
2020-11-21 14:29:21 -0500

    Adding a basic blogging engine and initial config

--- Gemfile
+++ Gemfile
@@ -2,9 +2,16 @@ source 'https://rubygems.org'
 
 gem 'middleman', '~> 4.2'
 gem 'middleman-autoprefixer', '~> 2.7'
+gem 'middleman-blog'
 gem 'middleman-livereload'
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby, :x64_mingw]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw, :x64_mingw]
+
+# Was in the blog template, is that needed?
+#gem 'redcarpet', '~> 3.3', '>= 3.3.3'
+
+# For feed.xml.builder
+gem 'builder', '~> 3.0'
 
 gem 'pry'
 gem 'mini_racer' # Needed for faster css asset when serving locally. Autoprefixer seem pretty slow...
--- config.rb
+++ config.rb
@@ -5,6 +5,31 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  blog.layout = "blog"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "blog/tag.html"
+  blog.calendar_template = "blog/calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
 activate :livereload
 
 set :build_dir, 'docs'
@@ -16,6 +41,7 @@ set :build_dir, 'docs'
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page 'blog/*', layout: 'blog'
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
