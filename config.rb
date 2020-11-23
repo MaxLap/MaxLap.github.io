74fcac1ca4090d11f7c75e81c12e24d0fb5f9f47
Maxime Lapointe <hunter_spawn@hotmail.com>
2020-11-22 21:32:31 -0500

    Structuring the layouts and the site a little

--- config.rb
+++ config.rb
@@ -13,7 +13,7 @@ activate :blog do |blog|
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
-  blog.layout = "blog"
+  blog.layout = "blog_post"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
@@ -44,7 +44,6 @@ set :markdown, :fenced_code_blocks => true
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-page 'blog/*', layout: 'blog'
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
