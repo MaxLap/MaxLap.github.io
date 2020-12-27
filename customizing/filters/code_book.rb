924418a7c2b97fa554573f6314c4156f35bd0028
Maxime Lapointe <hunter_spawn@hotmail.com>
2020-12-27 11:48:45 -0500

    Codebook can have a %AFTER row And many look cleanups

--- customizing/filters/code_book.rb
+++ customizing/filters/code_book.rb
@@ -10,6 +10,7 @@ module Customizing::Filters::CodeBook
     rows.each do |row_content|
       next if row_content.blank?
 
+      row_content, sep, after = row_content.partition(/^\s*(%AFTER)\s*\n?/)
       text, sep, code = row_content.partition(/^\s*(%RUBY|%CODE)\s*\n?/)
       code = code.rstrip.encode(Encoding::UTF_8)
 
@@ -27,9 +28,17 @@ module Customizing::Filters::CodeBook
 
       html_text = Customizing::Filters::Content.render(text)
       highlighted_code = Middleman::Syntax::Highlighter.highlight(code, language)
-
-      parts << %(<div class="code-book__left">#{html_text}</div>)
-      parts << %(<div class="highlight code-book__right"><div class="code-book__right-wrapper">#{highlighted_code}</div></div>)
+      if after.present?
+        has_after_left = ' code-book__left--has-after'
+        has_after_right = ' code-book__right--has-after'
+      end
+      parts << %(<div class="code-book__left#{has_after_left}">#{html_text}</div>)
+      parts << %(<div class="highlight code-book__right#{has_after_right}"><div class="code-book__right-wrapper">#{highlighted_code}</div></div>)
+      if after.present?
+        html_after = Customizing::Filters::Content.render(after)
+        parts << %(<div class="code-book__left code-book__left--after">#{html_after}</div>)
+        parts << %(<div class="highlight code-book__right code-book__right--after"></div>)
+      end
     end
 
     parts << %(</div>)
