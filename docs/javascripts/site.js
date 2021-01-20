a462a71ba3d919246cd188d457254e47fad9069e
Maxime Lapointe <hunter_spawn@hotmail.com>
2021-01-19 20:52:50 -0500

    Putting the frame of the blog online

--- docs/javascripts/site.js
+++ docs/javascripts/site.js
@@ -1,1 +1,47 @@
 // This is where it all goes :)
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll('.spoiler-toggler').forEach(function(toggler) {
+    var target = bubblingNextElementSibling(toggler);
+
+    toggler.addEventListener('click', function() {
+      toggler.classList.toggle('spoiler-toggler--hidden');
+      target.classList.toggle('spoiler-target--displayed');
+    });
+
+    target.addEventListener('click', function() {
+      toggler.classList.toggle('spoiler-toggler--hidden');
+      target.classList.toggle('spoiler-target--displayed');
+    });
+  });
+
+  document.querySelectorAll('.code-book').forEach(function(code_book) {
+    code_book.addEventListener("mousedown", function(e) {
+      var current = e.target;
+      var prev;
+
+      while(current && current !== code_book) {
+        prev = current;
+        current = current.parentElement;
+      }
+
+      var column_number = Array.prototype.indexOf.call(code_book.children, prev) % 2 + 1;
+
+      if (column_number > 0) {
+        code_book.classList.remove('selecting-column-1');
+        code_book.classList.remove('selecting-column-2');
+        code_book.classList.add('selecting-column-' + column_number);
+      }
+    });
+  });
+});
+
+function bubblingNextElementSibling(elem) {
+  while (!elem.nextElementSibling) {
+    elem = elem.parentElement;
+    if (!elem) {
+      return elem;
+    }
+  }
+  return elem.nextElementSibling;
+}
